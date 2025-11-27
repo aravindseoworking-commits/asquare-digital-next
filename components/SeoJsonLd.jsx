72e5be4efd3ee88d2970a6dfc1cf5ed56c36f88a
aravindseoworking-commits <aravind.seoworking@gmail.com>
2025-11-27 12:18:49 +0530

    Delete components directory

--- components/SeoJsonLd.jsx
+++
@@ -1,7 +0,0 @@
-export default function SeoJsonLd({ data }) {
-  // data is raw JSON-LD object or array
-  const json = JSON.stringify(data);
-  return (
-    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: json }} />
-  );
-}
